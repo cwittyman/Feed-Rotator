63a51244e459d0b1f2a70dd1f7c52308032e16a3
Wes <wrich@thinkpyxl.com>
2012-07-10 12:24:23 -0400

    The actual files for the feed rotator.

--- js/jquery.feed-rotator.js
+++ js/jquery.feed-rotator.js
@@ -1,26 +1,43 @@
-/*
+/* 
+ * Copyright (C) 2012, Wes Rich, Chase Witt, Pyxl, Inc.
  * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to deal 
+ * in the Software without restriction, including without limitation the rights 
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+ * of the Software, and to permit persons to whom the Software is furnished to do 
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all 
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
  
 // jgFeed 
 (function($){
-  $.extend({
-    jGFeed : function(url, fnk, num, key){
-      // Make sure url to get is defined
-      if(url == null) return false;
-      // Build Google Feed API URL
-      var gurl = "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&callback=?&q="+url;
-      if(num != null) gurl += "&num="+num;
-      if(key != null) gurl += "&key="+key;
-      // AJAX request the API
-      $.getJSON(gurl, function(data){
-        if(typeof fnk == 'function')
-          fnk.call(this, data.responseData.feed);
-        else
-          return false;
-      });
-    }
-  });
+	$.extend({
+		jGFeed : function(url, fnk, num, key){
+			// Make sure url to get is defined
+			if(url == null) return false;
+			// Build Google Feed API URL
+			var gurl = "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&callback=?&q="+url;
+			if(num != null) gurl += "&num="+num;
+			if(key != null) gurl += "&key="+key;
+			// AJAX request the API
+			$.getJSON(gurl, function(data){
+				if(typeof fnk == 'function')
+					fnk.call(this, data.responseData.feed);
+				else
+					return false;
+			});
+		}
+	});
 })(jQuery);
 
 
@@ -34,15 +51,13 @@
 				feedUrl: '',					// XML Feed URL
 				dataType: 'jsonp',				// 'xml', 'json', 'jsonp', 'script', 'html'
 				useStaticTitle: false,
-				feedTitle: 'Title',				// XML Item to use as "Title"
+				feedTitle: 'title',				// XML Item to use as "Title"
 				staticTitle: '',				// Static Title, instead of using a value from the feed
-				feedContent: 'Content',			// XML Item to use as "Content"
-				smartBrief: true,
-				smartBriefUrl: 'http://www.smartbrief.com/servlet/rss?b=cea',
+				feedContent: 'summary',			// XML Item to use as "Content"
 
 				itemClass: '.newsfeed-item',
 				titleClass: '.title',
-				contentClass: '.content',
+				contentClass: '.feed-content',
 				currentItem: 'visible-item',
 				lineHeight: 20,
 				parentHeight: 25,
@@ -52,88 +67,86 @@
 				autoRotateDirection: 'up'		// 'up' or 'down'
 			}
 			var options = $.extend(defaults, options);
-			
+
 			var urlsToLinks = function(text) {
 				text = $("<p>"+text+"</p>").text();
-			    var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-			    return text.replace(exp,"<a target='_blank' href='$1'> Read More</a>"); 
+				var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+				return text.replace(exp," <a target='_blank' href='$1'>Read More</a>");
 			}
-			
+
 			// Do something if fetchData!
 			if (options.fetchData) {
-				$.getJSON(options.feedUrl, function(data) {
-					var outputContent = '';
-					var outputContent = '';
-					for (msgNum in data) {
-						var itemTitle, itemContent;
-						for (var key in data[msgNum]) {
-							if (data[msgNum].hasOwnProperty(key)) {
-								if (key == options.feedContent) {
-									itemContent = data[msgNum][key];
-								} else if (key == options.feedTitle) {
-									itemTitle = data[msgNum][key];
-								} else if (data[msgNum][key] == null) {
-									continue;
-								} else if (data[msgNum][key].hasOwnProperty(options.feedContent)) {
-									itemContent = data[msgNum][key][options.feedContent];
-								} else if (data[msgNum][key].hasOwnProperty(options.feedTitle)) {
-									itemTitle = data[msgNum][key][options.feedTitle];
+				if (options.dataType == 'jsonp') {
+					$.getJSON(options.feedUrl, function(data) {
+						var outputContent = '';
+						for (msgNum in data) {
+							var itemTitle, itemContent;
+							for (var key in data[msgNum]) {
+								if (data[msgNum].hasOwnProperty(key)) {
+									if (key == options.feedContent) {
+										itemContent = data[msgNum][key];
+									} else if (key == options.feedTitle) {
+										itemTitle = data[msgNum][key];
+									} else if (data[msgNum][key] == null) {
+										continue;
+									} else if (data[msgNum][key].hasOwnProperty(options.feedContent)) {
+										itemContent = data[msgNum][key][options.feedContent];
+									} else if (data[msgNum][key].hasOwnProperty(options.feedTitle)) {
+										itemTitle = data[msgNum][key][options.feedTitle];
+									}
 								}
 							}
+							if (options.useStaticTitle) {
+								itemTitle = options.staticTitle;
+							}
+							outputContent += '<div class="'+options.itemClass.substr(1)+'">' +
+									'<span class="'+options.titleClass.substr(1)+'">'+itemTitle+'</span>'+
+									'<span class="'+options.contentClass.substr(1)+'">'+urlsToLinks(itemContent)+'</span>'+
+								'</div>';
 						}
-						if (options.useStaticTitle) {
-							itemTitle = options.staticTitle;
-						}
-						outputContent += '<div class="'+options.itemClass.substr(1)+'">' +
-								'<span class="'+options.titleClass.substr(1)+'">'+itemTitle+'</span>'+
-								'<span class="'+options.contentClass.substr(1)+'">'+urlsToLinks(itemContent)+'</span>'+
-							'</div>';
-					}
-					// Set the new content and restart the feedRotator, with "fetchData" set to false.
-					$me.html(outputContent).feedRotator($.extend(options, {fetchData: false}));
-				});
-				return this;
+						// Set the new content and restart the feedRotator, with "fetchData" set to false
+						$me.html(outputContent).feedRotator($.extend(options, {fetchData: false}));
+					});
+					return this;
+				} else {
+					$.jGFeed(options.feedUrl,
+						function (feeds) {
+							if (!feeds) {		// Check for errors
+								return false;		// There was an error
+							}
+							var outputContent = '';
+							for (var i = 0; i<feeds.entries.length;i++) {
+								var itemTitle, itemContent;
+								if (options.useStaticTitle) {
+									itemTitle = options.staticTitle;
+								} else {
+									itemTitle = feeds.entries[i][options.feedTitle];
+								}
+												// Hack - Get rid of bad characters
+								itemContent = $.trim(feeds.entries[i][options.feedContent]).replace(/�/g,' ') + " ... " + feeds.entries[i].link;
+								outputContent += '<div class="'+options.itemClass.substr(1)+'">' +
+										'<span class="'+options.titleClass.substr(1)+'">'+itemTitle+'</span>'+
+										'<span class="'+options.contentClass.substr(1)+'">'+itemContent+'</span>'+
+									'</div>';
+							}
+							// Set the new content and restart the feedRotator, with "fetchData" set to false
+							$me.html(outputContent).feedRotator($.extend(options, {fetchData: false}));
+						},15);
+					return this;
+				}
 			} else {
 				this.find(options.contentClass).each(function() {
 					$(this).html(urlsToLinks($(this).html()));
 				});
 			}
-			
-			//If Smart Brief
-			if(options.smartBrief){
-				$.jGFeed(options.smartBriefUrl,
-					function(feeds){
-					// Check for errors
-					if(!feeds){
-						// there was an error
-						return false;
-					}
-					var str = "";
-					for(var i = 0; i<feeds.entries.length;i++)
-					{
-						var link = "<a href='" +feeds.entries[i].link + "' target='_blank'> Read More</a>";
-						str += '<div class="newsfeed-item"><span class="title">';
-						str += 'CEA SmartBrief';
-						str += '</span><span class="content">';
-						str += $.trim(feeds.entries[i].title).replace(/�/g,' ') + " ... " + feeds.entries[i].link;
-						//.substring(0, 100).split(" ").slice(0, -1).join(" ") 
-						str += '</span></div>';
-									
-						
-					}
-					// Set the new content and restart the feedRotator, with "smartBrief" set to false.
-					$me.html(str).feedRotator($.extend(options, {smartBrief: false}));
-	
-				},15); 			
-			}
-			
+
 			// If no items, stop. If only one, duplicate it so it scrolls well.
 			if (this.find(options.itemClass).length < 1) {
 				return this;
 			} else if (this.find(options.itemClass).length == 1) {
 				this.append(this.find(options.itemClass).clone());
 			}
-			
+
 			// Set initial parent height and overflow, so it displays nicely.
 			this.css({
 				'overflow': 'hidden',
@@ -167,7 +180,7 @@
 						'top': '0px'
 					});
 			}
-			
+
 			// Movement function - rotates to the next item down.
 			this.rotateUp = function() {
 				var cParent, cItem, cItemPosition, nParent, nPHeight, nItem, nPosition;
@@ -204,7 +217,7 @@
 				
 				return true;
 			}
-			
+
 			// Movement function - rotates to the next item up.
 			this.rotateDown = function() {
 				var cParent, cItem, cItemPosition, nParent, nPHeight, nItem, nPosition;
@@ -212,14 +225,14 @@
 				cItem = cParent.find(options.contentClass);
 				cItemHeight = cItem.height();
 				cItemPosition = parseInt(cItem.css('top'));
-				
+
 				if ((cItemHeight+cItemPosition-options.lineHeight) <= 0) {
 					if (cParent.next().length > 0) {
 						nParent = cParent.next();
 					} else {
 						nParent = $me.find(options.itemClass).eq(0);
 					}
-					
+
 					cParent.removeClass(options.currentItem);
 					nParent.addClass(options.currentItem);
 					$me.find(options.itemClass).animate({
@@ -234,15 +247,15 @@
 						'top': (cItemPosition-options.lineHeight)+'px'
 					}, options.transitionTime, options.easing);
 				}
-				
+
 				clearTimeout($me.slideTimer);
 				$me.slideTimer = null;
 				$me.slideTimer = setTimeout(function() { $me.rotateDown(); }, options.waitTime+options.transitionTime);
 				
 				return true;
 			}
-			
-			// Define initial slide timer, so it auto slides on page load.
+
+			// Define initial slide timer, so it auto slides on page load
 			this.slideTimer = setTimeout(function() {
 					if (options.autoRotateDirection == 'down') {
 						$me.rotateDown();
@@ -250,8 +263,8 @@
 						$me.rotateUp();
 					}
 				}, options.waitTime);
-			
-			// Pause on hover. Restart timer when mouse leaves the area.
+
+			// Pause on hover. Restart timer when mouse leaves the area
 			this.hover(function() {
 				clearTimeout($me.slideTimer);
 				$me.slideTimer = null;
@@ -264,11 +277,10 @@
 					}
 				}, options.waitTime+options.transitionTime);
 			});
-			
+
 			// Wrap it up!
 			return this;
 		}
 
 	});
-	
 })(jQuery);
